c3352fbd362697e5aa0f01877d1e47cfaaedb958
Xavier <gemparpanggih123@gmail.com>
2022-10-13 18:48:52 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 	<title>Kepegawaian</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
 
+	<div class="container">
 	<h2><a href="#">Kepegawaian PT Sinar Matahari Fana</a></h2>
 	<h3>Data Pegawai</h3>
 
@@ -13,29 +16,35 @@
 	<br/>
 	<br/>
 
-	<table border="1">
-		<tr>
-			<th>Nama</th>
-			<th>Jabatan</th>
-			<th>Umur</th>
-			<th>Alamat</th>
-			<th>Opsi</th>
-		</tr>
-		@foreach($pegawai as $p)
-		<tr>
-			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-			<td>
-				<a href="/pegawai/edit/{{ $p->id }}">Edit</a>
-				|
-				<a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-
+		<table border="1" class="table table-hover table-striped">
+			<tr>
+				<th>Nama</th>
+				<th>Jabatan</th>
+				<th>Umur</th>
+				<th>Alamat</th>
+				<th>Opsi</th>
+			</tr>
+			@foreach($pegawai as $p)
+			<tr>
+				<td>{{ $p->pegawai_nama }}</td>
+				<td>{{ $p->pegawai_jabatan }}</td>
+				<td>{{ $p->pegawai_umur }}</td>
+				<td>{{ $p->pegawai_alamat }}</td>
+				<td>
+					<a href="/pegawai/edit/{{ $p->id }}">Edit</a>
+					|
+					<a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
+				</td>
+			</tr>
+			@endforeach
+		</table>
+		@if($pegawai->hasPages())
+		<div class="card-footer">
+			{{$pegawai->links()}}
+		</div>
+		@endif
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
 </body>
 </html>
