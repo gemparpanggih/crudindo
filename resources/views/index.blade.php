a21643ccd2c8d58673ffde27108c3c9c73efcd43
Xavier <gemparpanggih123@gmail.com>
2022-10-15 10:21:50 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,41 +8,41 @@
 <body>
 
 	<div class="container py-5">
-	<h2><a href="#">Kepegawaian PT Sinar Matahari Fana</a></h2>
-	<h3>Data Pegawai</h3>
+		<h2><a href="#">Kepegawaian PT Sinar Matahari Fana</a></h2>
+		<h3>Data Pegawai</h3>
 
-	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+		<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
 
-	<br/>
-	<br/>
+		<br/>
+		<br/>
 
-		<table border="1" class="table table-hover table-striped">
-			<tr>
-				<th>Nama</th>
-				<th>Jabatan</th>
-				<th>Umur</th>
-				<th>Alamat</th>
-				<th>Opsi</th>
-			</tr>
-			@foreach($pegawai as $p)
-			<tr>
-				<td>{{ $p->pegawai_nama }}</td>
-				<td>{{ $p->pegawai_jabatan }}</td>
-				<td>{{ $p->pegawai_umur }}</td>
-				<td>{{ $p->pegawai_alamat }}</td>
-				<td>
-					<a href="/pegawai/edit/{{ $p->id }}">Edit</a>
-					|
-					<a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
-				</td>
-			</tr>
-			@endforeach
-		</table>
-		@if($pegawai->hasPages())
-		<div class="card-footer">
-			{{$pegawai->links()}}
-		</div>
-		@endif
+			<table border="1" class="table table-hover table-striped">
+				<tr>
+					<th>Nama</th>
+					<th>Jabatan</th>
+					<th>Umur</th>
+					<th>Alamat</th>
+					<th>Opsi</th>
+				</tr>
+				@foreach($pegawai as $p)
+				<tr>
+					<td>{{ $p->pegawai_nama }}</td>
+					<td>{{ $p->pegawai_jabatan }}</td>
+					<td>{{ $p->pegawai_umur }}</td>
+					<td>{{ $p->pegawai_alamat }}</td>
+					<td>
+						<a href="/pegawai/edit/{{ $p->id }}">Edit</a>
+						|
+						<a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
+					</td>
+				</tr>
+				@endforeach
+			</table>
+			@if($pegawai->hasPages())
+			<div class="card-footer">
+				{{$pegawai->links()}}
+			</div>
+			@endif
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
